efa161e37d5fbe0cce29f096008f3ce2b1317804
funkrider <ian_davies@hotmail.com>
2017-08-09 23:35:27 -0400

    Add a call to the spec'd score function to this test. Validate that teh total and passed are equal.

--- test/codebreaker/core_test.clj
+++ test/codebreaker/core_test.clj
@@ -1,7 +1,19 @@
 (ns codebreaker.core-test
   (:require [clojure.test :refer :all]
-            [codebreaker.core :refer :all]))
+            [codebreaker.core :refer :all]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]
+            [clojure.spec.test.alpha :as stest]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (let [result (stest/summarize-results (stest/check 'codebreaker.core/score))]
+    (testing "Test - Spec Generative test of score function."
+      (is (= (:total result) (:check-passed result))))))
+
+
+(comment
+
+  ;(run-tests 'codebreaker.core-test)
+  ;=> {:test 1, :pass 1, :fail 0, :error 0, :type :summary}
+
+  )
