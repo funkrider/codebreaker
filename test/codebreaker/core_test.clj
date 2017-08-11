feb740954b74e6bc2c7b4f175068b7068076dcc9
funkrider <ian_davies@hotmail.com>
2017-08-11 13:03:58 -0400

    Add a call to the spec'd score function to this test. Validate that teh total and passed are equal.

--- test/codebreaker/core_test.clj
+++ test/codebreaker/core_test.clj
@@ -8,10 +8,17 @@
 (deftest a-test
   (let [result (stest/summarize-results (stest/check 'codebreaker.core/score))]
     (testing "Test - Spec Generative test of score function."
+      (println "hiya!" (clojure.core/pr result))
       (is (= (:total result) (:check-passed result))))))
 
+(run-tests 'codebreaker.core-test)
 
 (comment
+
+  ; There is a but in Leiningen test runner that throws an error when you try
+  ; to summarise generated results. Fix isto run the tests in code first... or add
+  ; :monkeypatch-clojure-test false to project.clj
+  ; https://github.com/technomancy/leiningen/issues/2173
 
   ;(run-tests 'codebreaker.core-test)
   ;=> {:test 1, :pass 1, :fail 0, :error 0, :type :summary}
